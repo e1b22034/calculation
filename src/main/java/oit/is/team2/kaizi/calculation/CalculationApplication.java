affd48eb215dcb6661164c55db81516222779e16
e1b22103 <e1b22103@oit.ac.jp>
2024-10-01 14:32:09 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team2/kaizi/calculation/CalculationApplication.java
@@ -0,0 +1,13 @@
+package oit.is.team2.kaizi.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team2/kaizi/calculation/CalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team2.kaizi.calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
